c3f48d0e421ac81f1f5589fd5351468ac1806842
waimea-tjeggers <tjeggers@waimea.school.nz>
2025-05-21 12:22:42 +1200

    got everything working, about to delete

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,18 @@ def home():
 
     return render_template("pages/home.jinja", things=records)
 
+@app.get("/thing/<int:id>")
+def things(id):
+        response = supabase.table("things").select().eq("id",id).single().execute()
+        record = response.data
+
+        return render_template("pages/thing.jinja", thing=record)
+
 
 @app.errorhandler(404)
 def notFound(error):
     return render_template("pages/404.jinja")
+
+@app.get("/delete/<int:id>")
+def deleteThing(id)
+     #TODO!!!!!!
